b42e5e264feb8ed255f46e00b56b3ca695018281
riq-dev <luis.riq.henrique@gmail.com>
2022-03-24 19:35:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
+
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const conta2 = new ContaCorrente(102, cliente2);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
+
+const contaPoupanca = new ContaPoupanca(50, cliente1, 9001);
+
+console.log(contaCorrenteRicardo);
+console.log(contaPoupanca);
