d644215792283959c01986d5276636cb4d34b299
riq-dev <luis.riq.henrique@gmail.com>
2022-03-25 22:46:10 -0400

    Conta poupanca adicionado

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 const cliente2 = new Cliente("Alice", 88822233309);
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-const conta2 = new ContaCorrente(102, cliente2);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const contaCorrente = new Conta(0, cliente1, 1001);
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 9001);
+contaCorrente.depositar(500);
+contaCorrente.sacar(100);
 
-console.log(contaCorrenteRicardo);
+const contaPoupanca = new Conta(50, cliente1, 9001);
+
+console.log(contaCorrente);
 console.log(contaPoupanca);
