0e0e870fdd89b55a64ef12fb94cdbdcb07a8258f
riq-dev <luis.riq.henrique@gmail.com>
2022-03-29 08:07:09 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
-
-const cliente1 = new Cliente("Ricardo", 11122233309);
-
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
